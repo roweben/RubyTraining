8ef07747c02d9d7b840e150331be530ac64fdcc7
Benjamin E. Rowe <roweben@excite.com>
2013-06-11 13:05:53 -0700

    Lesson 6.2

---
+++ format_text.rb
@@ -0,0 +1,8 @@
+line_width = 45
+first_col = (line_width/3)*2
+second_col = line_width/3
+puts("Table of Contents".center(line_width))
+puts("")
+puts("Chapter 1:  Getting Started".ljust(first_col) + "page  1".rjust(second_col))
+puts("Chapter 2:  Numbers".ljust(first_col) + "page  9".rjust(second_col))
+puts("Chapter 3:  Letters".ljust(first_col) + "page 13".rjust(second_col))
