0f0ee924b3c77e328e15676fe4e8bc9501a61762
Benjamin E. Rowe <roweben@excite.com>
2013-06-13 11:31:28 -0700

    Minor Revisions

--- leap_years.rb
+++ leap_years.rb
@@ -1,10 +1,8 @@
 puts('This program returns leap years.')
 print('Enter your starting year:  ')
-starting_year = gets.chomp
+istarting_year = gets.chomp.to_i
 print('Enter your ending year:  ')
-ending_year = gets.chomp
-istarting_year = starting_year.to_i
-iending_year = ending_year.to_i
+iending_year = gets.chomp.to_i
 while (istarting_year < (iending_year+1))
 	if (istarting_year%100==0) then
 		if (istarting_year%400 == 0) then
