2caef24c3dd17a2d7712fda391cea2110c68baee
Benjamin E. Rowe <roweben@excite.com>
2013-06-23 13:46:47 -0700

    More Revisions

--- english_number.rb
+++ english_number.rb
@@ -1,3 +1,4 @@
+
 def english_number number
 	if number < 0 # No negative numbers.
 		return 'Please enter a number that isn\'t negative.'
@@ -23,18 +24,13 @@ def english_number number
 	num_units =	['thousand', 'million', 'billion', 'trillion',
 				'quadrillion', 'quintillion', 'sextillion', 
 				'septillion', 'octillion', 'nonillion', 'decillion']
-	# "left" is how much of the number
-	# we still have left to write out.
-	# "write" is the part we are
-	# writing out right now.
-	# write and left...get it? :)
 	number_string = number.to_s
 	partial_segment = number_string.length%3
 	num_segments = []
 	y=0
     if partial_segment > 0
     	num_segments.push(number_string[y,partial_segment].to_s)
-    	y=y+partial_segment+1
+    	y=y+partial_segment
     end
     while y<number_string.length
     	num_segments.push(number_string[y,3])
@@ -42,13 +38,17 @@ def english_number number
     end
     x=0
 
+	# "left" is how much of the number
+	# we still have left to write out.
+	# "write" is the part we are
+	# writing out right now.
+	# write and left...get it? :)
     while x < num_segments.length
     	if num_segments[x].to_i > 0
-    		puts(num_segments[x])
-			write = num_segments[x].to_i/100 # How many hundreds?
+    		write = num_segments[x].to_i/100 # How many hundreds?
 			left = num_segments[x].to_i - write*100 # Subtract off those hundreds.
 			if write >= 1
-				num_string = num_string + ones_place[write-1] + ' hundred'
+				num_string = num_string + ones_place[write-1] + ' hundred '
 			end
 			write = left/10 # How many tens left?
 			left = left - write*10 # Subtract off those tens.
@@ -91,3 +91,19 @@ def english_number number
 end
 
 puts english_number(20070000035)
+puts english_number( 0)
+puts english_number( 9)
+puts english_number( 10)
+puts english_number( 11)
+puts english_number( 17)
+puts english_number( 32)
+puts english_number( 88)
+puts english_number( 99)
+puts english_number(100)
+puts english_number(101)
+puts english_number(234)
+puts english_number(3211)
+puts english_number(999999)
+puts english_number(1000000000000)
+puts english_number(1001000010000)
+puts english_number(10000000090009000009000008)
